19e989a3613647572b4964bdc79fb39c2fc7f867
quanghoang01 <haoquangbk@gmail.com>
2017-05-09 16:38:32 +0700

    Memory Card 1

---
+++ BT Memory Card 1/script.js
@@ -0,0 +1,54 @@
+var cards = ['f1','f2','f3','f4','f5'];
+var current = null;
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function flip(card){
+    $(card).css('pointer-events', 'none');
+    $(card).toggleClass('flipped');
+    
+    if (!current) {
+        current = $(card);    
+    } else {
+        $('.card').css('pointer-events', 'none');
+        if (current.attr('data-name') != $(card).attr('data-name')) {
+            setTimeout(function(){
+                current.toggleClass('flipped');
+                $(card).toggleClass('flipped');
+                current = null;
+                $('.card').css('pointer-events', 'auto');
+            },500);
+        } else {
+            setTimeout(function(){                             
+                current.css('opacity','0');
+                $(card).css('opacity','0');
+                current = null;
+                $('.card').css('pointer-events', 'auto');
+            },500);
+        }
+    }
+}
+
+$(function() {
+    cards = cards.concat(cards);
+    cards = shuffle(cards);
+    var html='';
+    for (var i=0;i<cards.length;i++){
+        html+='<div class="card" data-name="' + cards[i] + '" onclick = "flip(this)">' + 
+        '<img class="back" src="img/back.jpg"/><img class="front" src="img/' + cards[i] + '.jpg"/></div>';
+    }
+    $('.wrap').html(html);
+});
+
